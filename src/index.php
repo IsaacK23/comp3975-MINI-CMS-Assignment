e19ae37caf82a78cba7414a8b6ec862739bcce9e
calih33 <caliheung@gmail.com>
2026-02-05 11:41:18 -0800

    Simple login page with admin authentication that redirects to index.php page with "Hello World" on the page.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,15 @@
-<?php 
-include("index_db_params.php");
-
-// 1. AUTO-CREATE DATABASE 
-mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS `$db_name` ");
-
-// 2. Select the database now that we are sure it exists
-mysqli_select_db($conn, $db_name);
+<?php session_start(); 
+if (!isset($_SESSION['authenticated'])) {
+    header("Location: login.php");
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
 <h1>Hello World</h1>
-
-<?php if (!$conn): ?>
-    <p>Connection failed: <?php echo mysqli_connect_error(); ?></p>
-<?php else: ?>
-    <p>Success! You are connected to the <strong><?php echo $db_name; ?></strong> database.</p>
-    
-    <p>Your PHP server is currently looking at: 
-       <code>/Users/cali/Desktop/comp3975-MINI-CMS-Assignment/src/</code>
-    </p>
-<?php endif; ?>
+</html>
--- src/index_db_params.php
+++ src/index_db_params.php
@@ -3,10 +3,18 @@ getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="host.docker
 getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port="3333";
 getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $db_user="root";
 getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="secret";
-getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="New Database";
+getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="CMSDB";
+
+if (strlen( $db_name ) === 0) {
+    $conn = new mysqli("$db_host:$db_port", $db_user, $db_pass);
+    $db_name = "CMSDB";
+} else {
+    $conn = new mysqli("$db_host:$db_port", $db_user, $db_pass, $db_name);
+}
 
 // list of db params to different tables
 $conn = mysqli_connect("$db_host:$db_port", $db_user, $db_pass);
+
 
 // Check connection
 if (!$conn) {
